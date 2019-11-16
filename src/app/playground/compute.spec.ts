0469e0f2209611378b6d97c20c684d60295044d5
Vitalii Kravchyshyn <vitalik1991ua@gmail.com>
2019-11-17 01:18:58 +0200

    [Tests] Create base projects and add first unit tests

---
+++ src/app/playground/compute.spec.ts
@@ -0,0 +1,14 @@
+import {compute} from './compute';
+
+describe('', () => {
+
+    it('should return 0 if negative', () => {
+        const result = compute(-1)
+
+        expect(result).toBe(0)
+    })
+
+    it('shoud increment input if positive', () => {
+        expect(compute(10)).toEqual(11)
+    })
+})
