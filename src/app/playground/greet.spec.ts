410b6241a5f5d395dff24e1057f140eb6bad9626
Vitalii Kravchyshyn <vitalik1991ua@gmail.com>
2019-11-17 02:21:39 +0200

    [Test] remove duplicated test

--- src/app/playground/greet.spec.ts
+++ src/app/playground/greet.spec.ts
@@ -4,8 +4,4 @@ describe('greet', () => {
     it('should include name in returned message', () => {
         expect(greet('Nastya')).toContain('Nastya')
     })
-
-    it('should include name in returned message', () => {
-        expect(greet('Nastya')).toContain('Nastya')
-    })
 })
